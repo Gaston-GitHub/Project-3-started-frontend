5ae31fbcb288628f6b12d6cdd07d9eee7d6a0ea1
Gaston <gaston@github.com>
2021-07-27 00:24:18 +0200

    home page in progress

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -41,17 +41,17 @@ class Login extends Component {
 		<div className="relative px-4 py-10 bg-transparent shadow-lg sm:rounded-3xl sm:p-20">
 			<div className="max-w-md mx-auto">
 				<div>
-					<h1 className="text-2xl font-display-headr">Login</h1>
+					<h1 className="text-2xl text-gray-700  font-semibold">Login</h1>
 				</div>
 				<div className="divide-y divide-gray-200">
 					<div className="py-8 text-base leading-6 space-y-4 sm:text-lg sm:leading-7">
 						<div className="relative">
 							<input autoComplete="off" id="user name" name="username" value={username} type="text" onChange={this.handleChange} className="placeholder-transparent w-full h-10 text-gray-700 focus:outline-none" placeholder="user name" />
-							<label htmlFor="username" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm text-gray-700">User name!</label>
+							<label htmlFor="username" className="absolute left-0 -top-3.5 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm text-gray-700">User name!</label>
 						</div>
 						<div className="relative">
 							<input autoComplete="off" id="password" name="password" value={password} type="password" onChange={this.handleChange} className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
-							<label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Pas sword</label>
+							<label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Pas sword!</label>
 						</div>
 						<div className="relative">
 							<button onClick={this.handleFormSubmit} className="bg-red-900 text-white rounded-md px-2 py-1 font-mono">Submit</button>
--- src/pages/Private.js
+++ src/pages/Private.js
@@ -4,9 +4,25 @@ import React, { Component } from 'react';
 class Private extends Component {
 	render() {
 		return (
-			<div className="md:flex justify-center">
-				<h1 className="flex align-center">Welcome username</h1>
+			<div className="md:flex justify-center py-4">
+				<h1 className="md:flex text-center text-4xl">Wine Saver </h1>
+					<br></br>
+					<br></br>
+					<p className="md:flex text-center">Save your favorites wines</p>
+					<br></br>
+					<p className="md:flex text-center">How it works</p>	
+					<br></br>
+					<p className="4xl text-left px-10">
+						
+					</p>
+					<div className="md:flex-row">
+						<img className="object-contain md:object-scale-down h-24 w-full"  src="/img/how it works 1.png"></img>
+						<img className="object-contain md:object-scale-down h-24 w-full" src="/img/how it works 2.png"></img>
+						<img className="object-contain md:object-scale-down h-24 w-full" src="/img/how it works 3.png"></img>
+						<p className="text-center">Discover your taste profile.</p>
+					</div>
 			</div>
+			
 		);
 	}
 }
